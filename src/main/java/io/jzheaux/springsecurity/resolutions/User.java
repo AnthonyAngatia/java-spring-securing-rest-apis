09471e4e1df8184d5edc58293065bcda1087b4e4
anthony angatia <anthonyangatia@gmail.com>
2022-10-13 16:42:43 +0300

    [Feature]Use of Authorization/Roles

--- src/main/java/io/jzheaux/springsecurity/resolutions/User.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/User.java
@@ -1,9 +1,10 @@
 package io.jzheaux.springsecurity.resolutions;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.UUID;
 
 @Entity(name = "users")
@@ -19,6 +20,19 @@ public class User implements Serializable {
 
     @Column
     boolean enabled = true;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    Collection<UserAuthority> userAuthorities = new ArrayList<>();
+
+    public Collection<UserAuthority> getUserAuthorities(){
+        return Collections.unmodifiableCollection(this.userAuthorities);
+    }
+
+    public void grantAuthority(String authority){
+        UserAuthority userAuthority = new UserAuthority(this, authority);
+        this.userAuthorities.add(userAuthority);
+    }
+
 
     public User() {
     }
